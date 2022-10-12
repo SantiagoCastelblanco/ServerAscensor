20e2e99886663cc59761772759a76a96d3a779b6
Estudiantes <Estudiantes@LenovoM83V3>
2022-10-12 13:14:28 -0500

    Pisos

--- ServerAscensor/src/logica/AscensorLogica.java
+++ ServerAscensor/src/logica/AscensorLogica.java
@@ -2,20 +2,22 @@ package logica;
 
 
 public class AscensorLogica {
-     private int pisoActual;//por ahora lo declaro aqui
+    private Pisos[] listaPisos;
+    private int pisoActual;//por ahora lo declaro aqui
     public final int NUM_PISOS = 10;
     public final int PISOS_SUBTERRANEOS = 3;
     public final float cargaMaxima = 1000;//kilos
     private float carga = 0;
     
+    public AscensorLogica(){
+        listaPisos = new Pisos[NUM_PISOS];
+        pisoActual=0;
+    }
+    
     public void subirPiso(){
-        //for (int i = 0; i <=NUM_PISOS; i++) {
-            if(pisoActual<NUM_PISOS&&carga<=cargaMaxima){
-                pisoActual++;
-            }else{
-                //no se mueve el ascensor
-            }
-        //}
+        if(pisoActual<listaPisos.length-1&&carga<cargaMaxima){
+            pisoActual++;
+        }
     }
 
     public int getPisoActual() {
